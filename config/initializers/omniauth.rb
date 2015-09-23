83bd1172279f233de563af3291e99367d40bb8d1
Drew Conly <atconly@gmail.com>
2015-09-23 15:24:47 -0600

    add conditional for logout

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,4 +3,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
            image_size: 'large',
            scope: 'email',
            info_fields: 'email,first_name,last_name,name,link'
+
+  provider :twitter, ENV['twitter_id'], ENV['twitter_secret']
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root                            to: 'welcome#index'
   get '/dashboard',               to: 'dashboard#show'
   get '/auth/facebook/callback',  to: 'sessions#create'
-  # get '/auth/twitter/callback',   to: 'sessions#create'
+  get '/auth/twitter/callback',   to: 'sessions#create'
   get '/logout',                  to: 'sessions#destroy'
 end
