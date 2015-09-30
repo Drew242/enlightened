43ff0a433d52fa9729f40e02a8870f73df72d0c6
Drew Conly <atconly@gmail.com>
2015-09-29 22:36:39 -0600

    add articles as user change some style

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,12 @@
 class ArticlesController < ApplicationController
+  def show
+    @article = current_user.articles.find(params[:id])
+  end
+
   def create
+    user    = User.find_by(id: session[:user_id])
+    article = Wikipedia.find(params[:title])
+    user.articles.create(title: article.title, url: article.fullurl, body: article.text, privacy: 1)
+    redirect_to dashboard_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'articles/create'
-
   root                            to: 'welcome#index'
   get '/dashboard',               to: 'dashboard#show'
   get '/auth/facebook/callback',  to: 'sessions#create'
   get '/auth/twitter/callback',   to: 'sessions#create'
   get '/logout',                  to: 'sessions#destroy'
   get '/search',                  to: 'dashboard#search'
+  post '/articles/create',        to: 'articles#create'
+  resources :articles, only: [:show]
 end
